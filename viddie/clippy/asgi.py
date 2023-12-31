685e8758503cc4c702d92bf16018d61e032df4f0
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2023-12-31 19:54:51 +0200

    GNU

--- viddie/clippy/asgi.py
+++ viddie/clippy/asgi.py
@@ -1,11 +1,14 @@
 """
-ASGI config for clippy project.
+Viddie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+Viddie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 """
+
 
 import os
 
--- viddie/videomanagement/utils/tts_utils.py
+++ viddie/videomanagement/utils/tts_utils.py
@@ -1,9 +1,19 @@
+"""
+Viddie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Viddie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+
 from TTS.utils.synthesizer import Synthesizer
 from TTS.utils.manage import ModelManager
 import os
 import pyttsx3
-
-"""#tts_models/en/jenny/jenny#"""
 
 
 def create_model(model_path=rf"{os.path.abspath(os.getcwd())}\.models.json",
